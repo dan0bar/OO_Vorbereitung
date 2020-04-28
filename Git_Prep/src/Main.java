322549a8a5a5586a0f564856a81edd5562f13f2a
aleksandrgrek <goldoner@gmail.com>
2020-04-28 18:55:09 +0200

    Useless shit added

--- Git_Prep/src/Main.java
+++ Git_Prep/src/Main.java
@@ -2,6 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("privet moy drug");
+        System.out.println("ZDRASTE ZABOR PORKASTE"); System.out.println("privet moy drug");
+        System.out.println("ZDRASTE ZABOR PORKASTE"); System.out.println("privet moy drug");
+        System.out.println("ZDRASTE ZABOR PORKASTE"); System.out.println("privet moy drug");
+        System.out.println("ZDRASTE ZABOR PORKASTE"); System.out.println("privet moy drug");
+        System.out.println("ZDRASTE ZABOR PORKASTE"); System.out.println("privet moy drug");
+        System.out.println("ZDRASTE ZABOR PORKASTE"); System.out.println("privet moy drug");
+        System.out.println("ZDRASTE ZABOR PORKASTE"); System.out.println("privet moy drug");
         System.out.println("ZDRASTE ZABOR PORKASTE");
 
     }
